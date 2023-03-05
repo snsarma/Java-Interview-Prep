bbb30a6e32d5561c76968965fa96a30632d63b8f
Shravya Narayan Sarma <shravya.sarma@gmail.com>
2023-03-04 23:18:12 -0500

    First Commit

---
+++ TestString.java
@@ -0,0 +1,16 @@
+import java.lang.String;
+
+public class TestString {
+
+    public static void main(String[] args) {
+        String inputString = "I am here!";
+        String outputString = "";
+        char ch ;
+
+        for(int i =inputString.length()-1 ; i >=0 ; --i){
+            ch= inputString.charAt(i);
+            outputString = outputString+ch;
+        }
+        System.out.println("Reversed String:"+outputString);
+    }
+}
